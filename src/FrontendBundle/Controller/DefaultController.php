6eb3c45cae669c8a60019bdedb963fdbfa94f210
Gorka Gonzalez Etxepare <ggonzalez@pasaia.net>
2016-06-08 23:22:53 +0200

    frontend...

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -4,14 +4,53 @@ namespace FrontendBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Zerbikat\BackendBundle\Entity\Fitxa;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/{udala}/")
      */
-    public function indexAction()
+//    public function indexAction()
+//    {
+//        return $this->render('FrontendBundle:Default:index.html.twig');
+//    }
+
+//    /**
+//     * Lists all Fitxa entities.
+//     *
+//     * @Route("/frontend/", name="fitxa_index")
+//     * @Method("GET")
+//     */
+    public function indexAction($udala)
     {
-        return $this->render('FrontendBundle:Default:index.html.twig');
+        $em = $this->getDoctrine()->getManager();
+
+//        $fitxas = $em->getRepository('BackendBundle:Fitxa')->findAll();
+
+        $query = $em->createQuery('
+          SELECT f FROM BackendBundle:Fitxa f
+            WHERE f.udala = :udala
+            ORDER BY f.kontsultak DESC 
+        ');
+        $query->setParameter('udala', $udala);
+        $fitxak = $query->getResult();
+
+
+        $query = $em->createQuery('
+          SELECT f FROM BackendBundle:Familia f
+            WHERE f.udala = :udala
+        ');
+        $query->setParameter('udala', $udala);
+        $familiak = $query->getResult();
+
+        dump($familiak);
+
+//        return $this->render('FrontendBundle:Default:index.html.twig', array(
+        return $this->render('frontend\index.html.twig', array(
+            'fitxak' => $fitxak,
+            'familiak' => $familiak,
+        ));
     }
+
 }
