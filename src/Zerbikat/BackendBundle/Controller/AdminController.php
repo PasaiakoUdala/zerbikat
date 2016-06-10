32c3ae90c2cb724c51ed0ef49a6e7ea951713d0f
Gorka Gonzalez <ggonzalez@pasaia.net>
2016-06-10 11:06:06 +0200

    easy admin controller aldatzen hasi baino lehen

---
+++ src/Zerbikat/BackendBundle/Controller/AdminController.php
@@ -0,0 +1,10 @@
+<?php
+// src/Zerbikat/BackendBundle/Controller/AdminController.php
+namespace Zerbikat\BackendBundle\Controller;
+
+use JavierEguiluz\Bundle\EasyAdminBundle\Controller\AdminController as BaseAdminController;
+
+class AdminController extends BaseAdminController
+{
+// ...
+}
