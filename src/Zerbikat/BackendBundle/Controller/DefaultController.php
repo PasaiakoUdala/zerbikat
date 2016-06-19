20a788d43169a1dc60e0dba2f319770431ad8123
Gorka Gonzalez Etxepare <ggonzalez@pasaia.net>
2016-06-19 22:43:28 +0200

    ...

--- src/Zerbikat/BackendBundle/Controller/DefaultController.php
+++ src/Zerbikat/BackendBundle/Controller/DefaultController.php
@@ -9,7 +9,23 @@ class DefaultController extends Controller
     public function indexAction()
     {
 
-
+//        $query = $em->createQuery('
+//            SELECT f.oharraktext,f.helburuatext,f.ebazpensinpli,f.arduraaitorpena,f.aurreikusi,f.arrunta,f.isiltasunadmin,f.norkeskatutext,f.norkeskatutable,f.dokumentazioatext,f.dokumentazioatable,f.kostuatext,f.kostuatable,f.araudiatext,f.araudiatable,f.prozeduratext,f.prozeduratable,f.doklaguntext,f.doklaguntable,f.datuenbabesatext,f.datuenbabesatable,f.norkebatzitext,f.norkebatzitable,f.besteak1text,f.besteak1table,f.besteak2text,f.besteak2table,f.besteak3text,f.besteak3table,f.kanalatext,f.kanalatable,f.azpisailatable
+//              FROM BackendBundle:Eremuak f
+//          ');
+//        //$eremuak = $query->getResult();
+//        $eremu = $query->getSingleResult();
+//
+//        $query = $em->createQuery('
+//            SELECT f.oharraklabeleu,f.oharraklabeles,f.helburualabeleu,f.helburualabeles,f.ebazpensinplilabeleu,f.ebazpensinplilabeles,f.arduraaitorpenalabeleu,f.arduraaitorpenalabeles,f.aurreikusilabeleu,f.aurreikusilabeles,f.arruntalabeleu,f.arruntalabeles,f.isiltasunadminlabeleu,f.isiltasunadminlabeles,f.norkeskatulabeleu,f.norkeskatulabeles,f.dokumentazioalabeleu,f.dokumentazioalabeles,f.kostualabeleu,f.kostualabeles,f.araudialabeleu,f.araudialabeles,f.prozeduralabeleu,f.prozeduralabeles,f.doklagunlabeleu,f.doklagunlabeles,f.datuenbabesalabeleu,f.datuenbabesalabeles,f.norkebatzilabeleu,f.norkebatzilabeles,f.besteak1labeleu,f.besteak1labeles,f.besteak2labeleu,f.besteak2labeles,f.besteak3labeleu,f.besteak3labeles,f.kanalalabeleu,f.kanalalabeles,f.epealabeleu,f.epealabeles,f.doanlabeleu,f.doanlabeles,f.azpisailalabeleu,f.azpisailalabeles
+//              FROM BackendBundle:Eremuak f
+//          ');
+//        $label = $query->getSingleResult();
+//        return $this->render('BackendBundle:Default:index.html.twig',array(
+//            'eremu' => $eremu,
+//            'label' => $label
+//        ));
         return $this->render('BackendBundle:Default:index.html.twig');
+
     }
 }
