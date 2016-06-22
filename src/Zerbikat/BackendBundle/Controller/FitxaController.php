1323457a13de88e50fa131eeaa1e3301b7f7d329
Gorka Gonzalez <ggonzalez@pasaia.net>
2016-06-22 21:55:34 +0200

    tcpdf...

--- src/Zerbikat/BackendBundle/Controller/FitxaController.php
+++ src/Zerbikat/BackendBundle/Controller/FitxaController.php
@@ -154,22 +154,11 @@ class FitxaController extends Controller
         ');
         $labelak = $query->getSingleResult();
 
-//        return $this->render('fitxa/show.html.twig', array(
-//            'fitxa' => $fitxa,
-//            'kanalmotak'=>$kanalmotak,
-//            'delete_form' => $deleteForm->createView(),
-//            'eremuak'=> $eremuak,
-//            'labelak'=> $labelak
-//        ));
-        $html="<table border=1><tr><td>aaaaaaa</td><td>hola</td></tr>";
+
+//        $html="<table border=1><tr><td>aaaaaaa</td><td>hola</td></tr>";
 
         $html="<table border=\"1\"  cellpadding=\"4\">
-        <tr>
-            <td colspan=\"12\" align=\"right\">
-               <span style=\"font-size: xx-small;\">Azken eguneraketa:</span> 
-            </td>
-        </tr>
-        <tr>
+        <thead><tr>
             <th colspan=\"2\">
                         <img src='".$fitxa->getUdala()->getLogoa()."' />
            
@@ -182,28 +171,288 @@ class FitxaController extends Controller
                 <h4>".$fitxa->getDeskribapenaeu()."</h4>
                 <h5>".$fitxa->getDeskribapenaes()."</h5>
             </th>
-        </tr>
+        </tr></thead>
     </table>
         ";
+//        dump($eremuak);
+        /* HELBURUA */
+        if ($eremuak['helburuatext'])
+        {
+            $html=$html."<br /><br /><table cellspacing=\"3\" cellpadding=\"3\">
+                    <thead><tr>
+                        <th style=\"text-align:center\">".$labelak['helburualabeleu']."</th>
+                        <th style=\"text-align:center\">".$labelak['helburualabeles']."</th>
+                    </tr></thead>
+                    <tr>
+                        <td><span style=\"font-size: small;\">".$fitxa->getHelburuaeu()."</span></td>
+                        <td><span style=\"font-size: small;\">".$fitxa->getHelburuaes()."</span></td>
+                    </tr>
+                    </table>";
+        }
 
-    $html=$html."<br /><br /><table cellspacing=\"3\" cellpadding=\"3\"><tr>
-                    <th style=\"text-align:center\">ZER DA? ZERTARAKO DA?</th>
-                    <th style=\"text-align:center\">¿QUÉ ES? ¿PARA QUÉ?</th>
-                </tr>
-                <tr>
-                <td><span style=\"font-size: small;\">".$fitxa->getHelburuaeu()."</span></td>
-                <td><span style=\"font-size: small;\">".$fitxa->getHelburuaes()."</span></td>
-                </tr>
-                
-                </table>
-    ";
+        /* NORK ESKA DEZAKE */
+        if (($eremuak['norkeskatutext'])||($eremuak['norkeskatutable']))
+        {
+            $html=$html."<table cellspacing=\"0\" cellpadding=\"0\">
+            <thead>
+            <tr>
+                <th style=\"text-align:center\">".$labelak['norkeskatulabeleu']."</th>
+                <th style=\"text-align:center\">".$labelak['norkeskatulabeles']."</th>
+            </tr></thead>
+            ";
+            $html = $html."<tr><td>";
+            if ($eremuak['norkeskatutext']) {
+                $html = $html."<span style=\"font-size: small;\">".$fitxa->getNorkeu()."</span>";
+            }
+            if ($eremuak['norkeskatutable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getNorkeskatuak() as $nork) {
+                        $html = $html."<li>".$nork->getNorkeskatu()->getNorkeu()."</li>";
+                }
+                $html = $html . "</ul></span>";
+            }
+            $html = $html."</td><td>";
+            if ($eremuak['norkeskatutext']) {
+                $html = $html."<span style=\"font-size: small;\">".$fitxa->getNorkes()."</span>";
+            }
+            if ($eremuak['norkeskatutable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getNorkeskatuak() as $nork) {
+                        $html = $html."<li>".$nork->getNorkeskatu()->getNorkes()."</li>";
+                }
+                $html = $html . "</ul></span>";
+            }
+            $html = $html."</td></tr>";
+            $html=$html."</table>";
+        }
 
 
+        /* DOKUMENTAZIOA  */
+        if (($eremuak['dokumentazioatext'])||($eremuak['dokumentazioatable']))
+        {
+            $html=$html."<table cellspacing=\"0\" cellpadding=\"0\">
+            <thead>
+            <tr>
+                <th style=\"text-align:center\">".$labelak['dokumentazioalabeleu']."</th>
+                <th style=\"text-align:center\">".$labelak['dokumentazioalabeles']."</th>
+            </tr></thead>
+            ";
+            $html = $html."<tr><td>";
+            if ($eremuak['dokumentazioatext']) {
+                $html = $html."
+                    <span style=\"font-size: small;\">".$fitxa->getDokumentazioaeu()."</span>";
+            }
+            if ($eremuak['dokumentazioatable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getDokumentazioak() as $doku) {
+                    if ($doku->getDokumentazioa()->getEstekaeu())
+                    {
+                        $html = $html."<li><a href=\"".$doku->getDokumentazioa()->getEstekaeu()."\">".$doku->getDokumentazioa()->getDeskribapenaeu()."</li></a>";
+                    }else
+                    {
+                        $html = $html."<li>".$doku->getDokumentazioa()->getDeskribapenaeu()."</li>";
+                    }
+                }
+                $html = $html . "</ul></span>";
+            }
+            $html = $html."</td><td>";
+            if ($eremuak['dokumentazioatext']) {
+                $html = $html."
+                    <span style=\"font-size: small;\">".$fitxa->getDokumentazioaes()."</span>";
+            }
+            if ($eremuak['dokumentazioatable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getDokumentazioak() as $doku) {
+                    if ($doku->getDokumentazioa()->getEstekaes())
+                    {
+                        $html = $html."<li><a href=\"".$doku->getDokumentazioa()->getEstekaes()."\">".$doku->getDokumentazioa()->getDeskribapenaes()."</li></a>";
+                    }else
+                    {
+                        $html = $html."<li>".$doku->getDokumentazioa()->getDeskribapenaes()."</li>";
+                    }
+                }
+                $html = $html . "</ul></span>";
+            }
+            $html = $html."</td></tr>";
+            $html=$html."</table>";
+        }
 
+        /* DOKLAGUN */
+        if (($eremuak['doklaguntext'])||($eremuak['doklaguntable']))
+        {
+            $html=$html."<table cellspacing=\"0\" cellpadding=\"0\">
+            <thead>
+            <tr>
+                <th style=\"text-align:center\">".$labelak['doklagunlabeleu']."</th>
+                <th style=\"text-align:center\">".$labelak['doklagunlabeles']."</th>
+            </tr></thead>
+            ";
+            $html = $html."<tr><td>";
+            if ($eremuak['doklaguntext']) {
+                $html = $html."
+                    <span style=\"font-size: small;\">".$fitxa->getDoklaguneu()."</span>";
+            }
+            if ($eremuak['doklaguntable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getDoklagunak() as $doku) {
+                    if ($doku->getDoklagun()->getEstekaeu())
+                    {
+                        $html = $html."<li><a href=\"".$doku->getDoklagun()->getEstekaeu()."\">".$doku->getDoklagun()->getDeskribapenaeu()."</li></a>";
+                    }else
+                    {
+                        $html = $html."<li>".$doku->getDoklagun()->getDeskribapenaeu()."</li>";
+                    }
+                }
+                $html = $html . "</ul></span>";
+            }
+            $html = $html."</td><td>";
+            if ($eremuak['doklaguntext']) {
+                $html = $html."
+                    <span style=\"font-size: small;\">".$fitxa->getDoklagunes()."</span>";
+            }
+            if ($eremuak['doklaguntable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getDoklagunak() as $doku) {
+                    if ($doku->getDoklagun()->getEstekaes())
+                    {
+                        $html = $html."<li><a href=\"".$doku->getDoklagun()->getEstekaes()."\">".$doku->getDoklagun()->getDeskribapenaes()."</li></a>";
+                    }else
+                    {
+                        $html = $html."<li>".$doku->getDoklagun()->getDeskribapenaes()."</li>";
+                    }
+                }
+                $html = $html . "</ul></span>";
+            }
+            $html = $html."</td></tr>";
+            $html=$html."</table>";
+        }
 
+        /* KANALAK */
+//        if (($eremuak['dokumentazioatext'])||($eremuak['dokumentazioatable']))
+//        {
+//            $html=$html."<table cellspacing=\"0\" cellpadding=\"0\">
+//        <thead>
+//        <tr>
+//            <th style=\"text-align:center\">".$labelak['dokumentazioalabeleu']."</th>
+//            <th style=\"text-align:center\">".$labelak['dokumentazioalabeles']."</th>
+//        </tr></thead>
+//        ";
+//            $html = $html."<tr><td>";
+//            if ($eremuak['dokumentazioatext']) {
+//                $html = $html."
+//                <span style=\"font-size: small;\">".$fitxa->getDokumentazioaeu()."</span>";
+//            }
+//            if ($eremuak['dokumentazioatable'])
+//            {
+//                $html = $html . "<span style=\"font-size: small;\"><ul>";
+//                foreach ($fitxa->getDokumentazioak() as $doku) {
+//                    if ($doku->getDokumentazioa()->getEstekaeu())
+//                    {
+//                        $html = $html."<li><a href=\"".$doku->getDokumentazioa()->getEstekaeu()."\">".$doku->getDokumentazioa()->getDeskribapenaeu()."</li></a>";
+//                    }else
+//                    {
+//                        $html = $html."<li>".$doku->getDokumentazioa()->getDeskribapenaeu()."</li>";
+//                    }
+//                }
+//                $html = $html . "</ul></span>";
+//            }
+//            $html = $html."</td><td>";
+//            if ($eremuak['dokumentazioatext']) {
+//                $html = $html."
+//                <span style=\"font-size: small;\">".$fitxa->getDokumentazioaes()."</span>";
+//            }
+//            if ($eremuak['dokumentazioatable'])
+//            {
+//                $html = $html . "<span style=\"font-size: small;\"><ul>";
+//                foreach ($fitxa->getDokumentazioak() as $doku) {
+//                    if ($doku->getDokumentazioa()->getEstekaes())
+//                    {
+//                        $html = $html."<li><a href=\"".$doku->getDokumentazioa()->getEstekaes()."\">".$doku->getDokumentazioa()->getDeskribapenaes()."</li></a>";
+//                    }else
+//                    {
+//                        $html = $html."<li>".$doku->getDokumentazioa()->getDeskribapenaes()."</li>";
+//                    }
+//                }
+//                $html = $html . "</ul></span>";
+//            }
+//            $html = $html."</td></tr>";
+//            $html=$html."</table>";
+//        }
 
+        /* KOSTU TAULAK */
+        if (($eremuak['kostuatext'])||($eremuak['kostuatable']))
+        {
+            $html=$html."<table cellspacing=\"0\" cellpadding=\"0\">
+        <thead>
+        <tr>
+            <th style=\"text-align:center\">".$labelak['kostualabeleu']."</th>
+            <th style=\"text-align:center\">".$labelak['kostualabeles']."</th>
+        </tr></thead>
+        ";
+            $html = $html."<tr><td>";
+            $kont=0;
+            if ($eremuak['kostuatable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getAzpiatalak() as $azpiatal)
+                {
+                    $kont++;
+                    $html=$html."<table border=\"1\">
+                        <tr>
+                            <th colspan=2>
+                                <a href='http://zergaordenantzak/kudeaketa.php/atala/show/id/".$azpiatal->getId()."' target=\"_blank\">".
+                                    $azpiatal->getAtala()->getOrdenantza()->getId().".".
+                                    $azpiatal->getAtala()->getId().".".
+                                    $azpiatal->getId().
+                                "</a>
+                            </th>
+                        </tr>";
 
+//                    if ($doku->getDokumentazioa()->getEstekaeu())
+//                    {
+//                        $html = $html."<li><a href=\"".$doku->getDokumentazioa()->getEstekaeu()."\">".$doku->getDokumentazioa()->getDeskribapenaeu()."</li></a>";
+//                    }else
+//                    {
+//                        $html = $html."<li>".$doku->getDokumentazioa()->getDeskribapenaeu()."</li>";
+//                    }
+                    $html=$html."</table>";
+                }
+                $html = $html . "</ul></span>";
+            }
+            if ($eremuak['kostuatext']) {
+                $html = $html."
+                <span style=\"font-size: small;\">".$fitxa->getKostuaeu()."</span>";
+            }
 
+            $html = $html."</td><td>";
+            $kont=0;
+            if ($eremuak['kostuatable'])
+            {
+                $html = $html . "<span style=\"font-size: small;\"><ul>";
+                foreach ($fitxa->getDokumentazioak() as $doku) {
+                    if ($doku->getDokumentazioa()->getEstekaes())
+                    {
+                        $html = $html."<li><a href=\"".$doku->getDokumentazioa()->getEstekaes()."\">".$doku->getDokumentazioa()->getDeskribapenaes()."</li></a>";
+                    }else
+                    {
+                        $html = $html."<li>".$doku->getDokumentazioa()->getDeskribapenaes()."</li>";
+                    }
+                }
+                $html = $html . "</ul></span>";
+            }
+            if ($eremuak['dokumentazioatext']) {
+                $html = $html."
+                <span style=\"font-size: small;\">".$fitxa->getKostuaes()."</span>";
+            }
+            $html = $html."</td></tr>";
+            $html=$html."</table>";
+        }
 
         $pdf = $this->get("white_october.tcpdf")->create('vertical', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
         $pdf->SetAuthor($this->getUser()->getUdala());
@@ -240,9 +489,9 @@ class FitxaController extends Controller
             $deleteForm = $this->createDeleteForm($fitxa);
             $editForm = $this->createForm('Zerbikat\BackendBundle\Form\FitxaType', $fitxa);
             $editForm->handleRequest($request);
-    
+
             $em = $this->getDoctrine()->getManager();
-    
+
             if ($editForm->isSubmitted() && $editForm->isValid())
             {
                 $fitxa->setUpdatedAt(new \DateTime());
@@ -255,13 +504,13 @@ class FitxaController extends Controller
                 FROM BackendBundle:Eremuak f
             ');
             $eremuak = $query->getSingleResult();
-    
+
             $query = $em->createQuery('
               SELECT f.oharraklabeleu,f.oharraklabeles,f.helburualabeleu,f.helburualabeles,f.ebazpensinplilabeleu,f.ebazpensinplilabeles,f.arduraaitorpenalabeleu,f.arduraaitorpenalabeles,f.aurreikusilabeleu,f.aurreikusilabeles,f.arruntalabeleu,f.arruntalabeles,f.isiltasunadminlabeleu,f.isiltasunadminlabeles,f.norkeskatulabeleu,f.norkeskatulabeles,f.dokumentazioalabeleu,f.dokumentazioalabeles,f.kostualabeleu,f.kostualabeles,f.araudialabeleu,f.araudialabeles,f.prozeduralabeleu,f.prozeduralabeles,f.doklagunlabeleu,f.doklagunlabeles,f.datuenbabesalabeleu,f.datuenbabesalabeles,f.norkebatzilabeleu,f.norkebatzilabeles,f.besteak1labeleu,f.besteak1labeles,f.besteak2labeleu,f.besteak2labeles,f.besteak3labeleu,f.besteak3labeles,f.kanalalabeleu,f.kanalalabeles,f.epealabeleu,f.epealabeles,f.doanlabeleu,f.doanlabeles,f.azpisailalabeleu,f.azpisailalabeles
                 FROM BackendBundle:Eremuak f
             ');
             $labelak = $query->getSingleResult();
-    
+
     //        }
 //            if ($fitxa->getUdala()==$this->getUser()->getUdala()) {
                 return $this->render('fitxa/edit.html.twig', array(
@@ -278,7 +527,7 @@ class FitxaController extends Controller
         }else
         {
             return $this->redirectToRoute('backend_errorea');
-        }            
+        }
     }
 
     /**
@@ -330,5 +579,5 @@ class FitxaController extends Controller
 
 
 
-    
+
 }
