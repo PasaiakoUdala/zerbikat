a65a480560dfca5ce0804520dd5a98509bf2d46f
Gorka Gonzalez Etxepare <ggonzalez@pasaia.net>
2016-07-02 11:42:53 +0200

    Fix. frontend-ean udalaren kodea erabili, ez id-a, show eta pdf

--- src/FrontendBundle/Controller/DefaultController.php
+++ src/FrontendBundle/Controller/DefaultController.php
@@ -79,8 +79,8 @@ class DefaultController extends Controller
 
         $query = $em->createQuery('
           SELECT f.oharraktext,f.helburuatext,f.ebazpensinpli,f.arduraaitorpena,f.aurreikusi,f.arrunta,f.isiltasunadmin,f.norkeskatutext,f.norkeskatutable,f.dokumentazioatext,f.dokumentazioatable,f.kostuatext,f.kostuatable,f.araudiatext,f.araudiatable,f.prozeduratext,f.prozeduratable,f.doklaguntext,f.doklaguntable,f.datuenbabesatext,f.datuenbabesatable,f.norkebatzitext,f.norkebatzitable,f.besteak1text,f.besteak1table,f.besteak2text,f.besteak2table,f.besteak3text,f.besteak3table,f.kanalatext,f.kanalatable,f.azpisailatable
-            FROM BackendBundle:Eremuak f
-            WHERE f.udala = :udala
+            FROM BackendBundle:Eremuak f LEFT JOIN BackendBundle:Udala u WITH f.udala=u.id
+            WHERE u.kodea = :udala
         ');
         $query->setParameter('udala', $udala);
         //$eremuak = $query->getResult();
@@ -88,8 +88,8 @@ class DefaultController extends Controller
 
         $query = $em->createQuery('
           SELECT f.oharraklabeleu,f.oharraklabeles,f.helburualabeleu,f.helburualabeles,f.ebazpensinplilabeleu,f.ebazpensinplilabeles,f.arduraaitorpenalabeleu,f.arduraaitorpenalabeles,f.aurreikusilabeleu,f.aurreikusilabeles,f.arruntalabeleu,f.arruntalabeles,f.isiltasunadminlabeleu,f.isiltasunadminlabeles,f.norkeskatulabeleu,f.norkeskatulabeles,f.dokumentazioalabeleu,f.dokumentazioalabeles,f.kostualabeleu,f.kostualabeles,f.araudialabeleu,f.araudialabeles,f.prozeduralabeleu,f.prozeduralabeles,f.doklagunlabeleu,f.doklagunlabeles,f.datuenbabesalabeleu,f.datuenbabesalabeles,f.norkebatzilabeleu,f.norkebatzilabeles,f.besteak1labeleu,f.besteak1labeles,f.besteak2labeleu,f.besteak2labeles,f.besteak3labeleu,f.besteak3labeles,f.kanalalabeleu,f.kanalalabeles,f.epealabeleu,f.epealabeles,f.doanlabeleu,f.doanlabeles,f.azpisailalabeleu,f.azpisailalabeles
-            FROM BackendBundle:Eremuak f
-            WHERE f.udala = :udala
+            FROM BackendBundle:Eremuak f LEFT JOIN BackendBundle:Udala u WITH f.udala=u.id
+            WHERE u.kodea = :udala
         ');
         $query->setParameter('udala', $udala);
         $labelak = $query->getSingleResult();
@@ -120,16 +120,16 @@ class DefaultController extends Controller
 
         $query = $em->createQuery('
           SELECT f.oharraktext,f.helburuatext,f.ebazpensinpli,f.arduraaitorpena,f.aurreikusi,f.arrunta,f.isiltasunadmin,f.norkeskatutext,f.norkeskatutable,f.dokumentazioatext,f.dokumentazioatable,f.kostuatext,f.kostuatable,f.araudiatext,f.araudiatable,f.prozeduratext,f.prozeduratable,f.doklaguntext,f.doklaguntable,f.datuenbabesatext,f.datuenbabesatable,f.norkebatzitext,f.norkebatzitable,f.besteak1text,f.besteak1table,f.besteak2text,f.besteak2table,f.besteak3text,f.besteak3table,f.kanalatext,f.kanalatable,f.azpisailatable
-            FROM BackendBundle:Eremuak f
-            WHERE f.udala = :udala
+            FROM BackendBundle:Eremuak f LEFT JOIN BackendBundle:Udala u WITH f.udala=u.id
+            WHERE u.kodea = :udala
         ');
         $query->setParameter('udala', $udala);
         $eremuak = $query->getSingleResult();
 
         $query = $em->createQuery('
           SELECT f.oharraklabeleu,f.oharraklabeles,f.helburualabeleu,f.helburualabeles,f.ebazpensinplilabeleu,f.ebazpensinplilabeles,f.arduraaitorpenalabeleu,f.arduraaitorpenalabeles,f.aurreikusilabeleu,f.aurreikusilabeles,f.arruntalabeleu,f.arruntalabeles,f.isiltasunadminlabeleu,f.isiltasunadminlabeles,f.norkeskatulabeleu,f.norkeskatulabeles,f.dokumentazioalabeleu,f.dokumentazioalabeles,f.kostualabeleu,f.kostualabeles,f.araudialabeleu,f.araudialabeles,f.prozeduralabeleu,f.prozeduralabeles,f.doklagunlabeleu,f.doklagunlabeles,f.datuenbabesalabeleu,f.datuenbabesalabeles,f.norkebatzilabeleu,f.norkebatzilabeles,f.besteak1labeleu,f.besteak1labeles,f.besteak2labeleu,f.besteak2labeles,f.besteak3labeleu,f.besteak3labeles,f.kanalalabeleu,f.kanalalabeles,f.epealabeleu,f.epealabeles,f.doanlabeleu,f.doanlabeles,f.azpisailalabeleu,f.azpisailalabeles
-            FROM BackendBundle:Eremuak f
-            WHERE f.udala = :udala
+            FROM BackendBundle:Eremuak f LEFT JOIN BackendBundle:Udala u WITH f.udala=u.id
+            WHERE u.kodea = :udala
         ');
         $query->setParameter('udala', $udala);
         $labelak = $query->getSingleResult();
@@ -143,7 +143,7 @@ class DefaultController extends Controller
         ));
 
         $pdf = $this->get("white_october.tcpdf")->create('vertical', PDF_UNIT, PDF_PAGE_FORMAT, true, 'UTF-8', false);
-        $pdf->SetAuthor($this->getUser()->getUdala());
+        $pdf->SetAuthor($udala);
 //        $pdf->SetTitle(('Our Code World Title'));
         $pdf->SetTitle(($fitxa->getDeskribapenaeu()));
         $pdf->SetSubject($fitxa->getDeskribapenaes());
