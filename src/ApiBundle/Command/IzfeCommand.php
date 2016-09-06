6ac098c741508db69adad195f2b7c5ae273ce4c8
Iker Ibarguren <ikerib@gmail.com>
2016-09-06 12:34:27 +0200

    IZFE Command froga

--- src/ApiBundle/Command/IzfeCommand.php
+++ src/ApiBundle/Command/IzfeCommand.php
@@ -50,20 +50,73 @@ class IzfeCommand extends ContainerAwareCommand
         ]);
 
         $idPagina=1;
-
-        $A204AYUNTA = $udalKodea;
+        $A204AYUNTA = "'" . $udalKodea . "'";
         $A204IDPAGINA = $idPagina;
-        $idPagina +=1;
 
-        $A204DENOMI = 'Home ' . $udala->getIzenaeu();
-        $A204TITCAST = 'Inicio ' . $udala->getIzenaes();
-        $A204TITEUSK = $udala->getIzenaeu().' Hasiera';
+
+        $A204DENOMI = "'Home " . $udala->getIzenaeu() . "'";
+        $A204TITCAST = "'Inicio " . $udala->getIzenaes() . "'";
+        $A204TITEUSK = "'" . $udala->getIzenaeu()." Hasiera'";
         $A204PUBLICADA = 1;
         $A204FECALTA = date('Ymd');
-        $A204TIPO = "HOME";
-        $A204IDTIPO = "USC";
-        $sql="INSER INTO paginas (A204AYUNTA,A204IDPAGINA,A204DENOMI,A204TITCAST,A204TITEUSK,A204PUBLICADA,A204FECALTA,A204TIPO,A204IDTIPO)
-            VALUES ($A204AYUNTA,$A204IDPAGINA, $A204DENOMI, $A204TITCAST, $A204TITEUSK, $A204PUBLICADA, $A204FECALTA, $A204TIPO, $A204IDTIPO)";
+        $A204TIPO = "'HOME'";
+        $A204IDTIPO = "'USC'";
+        $sql = "TRUNCATE TABLE `UDAT204`;\n"; // Orriak
+        $sql = $sql . "INSERT INTO UDAT204 (A204AYUNTA,A204IDPAGINA,A204DENOMI,A204TITCAST,A204TITEUSK,A204PUBLICADA,A204FECALTA,A204TIPO,A204IDTIPO)
+            VALUES ($A204AYUNTA,$A204IDPAGINA, $A204DENOMI, $A204TITCAST, $A204TITEUSK, $A204PUBLICADA, $A204FECALTA, $A204TIPO, $A204IDTIPO);\n";
+
+
+        // Blokeak
+
+        $query = $em->createQuery('
+          SELECT f FROM BackendBundle:Familia f LEFT JOIN BackendBundle:Udala u WITH f.udala=u.id
+            WHERE u.kodea = :udala
+        ');
+        $query->setParameter('udala', $udalKodea);
+        $familiak = $query->getResult();
+
+        $idBlokea=0;
+        $idOrden=0;
+        $sql = $sql . "TRUNCATE TABLE `UDAT203`;\n"; // Blokeak
+        $sql = $sql . "TRUNCATE TABLE `UDAT206`;\n"; // Orriak - Blokeak
+        foreach ($familiak as $f) {
+            $idBlokea += 1;
+            $idOrden +=1;
+            $output->writeln([
+                '',
+                'Blokeak sortzen... ' . $f->getFamiliaeu()
+            ]);
+
+            $A203AYUNTA = "'" . $udalKodea . "'";
+            $A203IDBLOQUE = $idBlokea;
+            $A203DENOMI = "'" . $idBlokea . " Blokea'";
+            $A203TITCAST = "'" . $f->getFamiliaes() . "'";
+            $A203TITEUSK = "'" . $f->getFamiliaeu() . "'";
+            $A203FECALTA = date('Ymd');
+
+
+            $sql = $sql . "INSERT INTO UDAT203 (A203AYUNTA,A203IDBLOQUE,A203DENOMI,A203TITCAST,A203TITEUSK,A203FECALTA)
+                VALUES($A203AYUNTA,$A203IDBLOQUE,$A203DENOMI,$A203TITCAST,$A203TITEUSK,$A203FECALTA);
+            ";
+
+            $output->writeln([
+                'Blokeak eta orriak erlazionatzen... ',
+                ''
+            ]);
+
+            $A206AYUNTA = "'" . $udalKodea . "'";
+            $A206IDPAGINA = $idPagina;
+            $A206IDBLOQUE = $idBlokea;
+            $A206ORDEN = $idOrden;
+            $A206VISUAL = 1;
+            $sql = $sql . "INSERT INTO UDAT206 (A206AYUNTA,A206IDPAGINA,A206IDBLOQUE,A206ORDEN,A206VISUAL)
+                VALUES($A206AYUNTA,$A206IDPAGINA,$A206IDBLOQUE,$A206ORDEN,$A206VISUAL);
+            ";
+        }
+
+
+        $idPagina +=1;
+
 
 
         // Fitxategia sortu
