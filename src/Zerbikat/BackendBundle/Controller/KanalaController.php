8b48d6b2f144b49fdb4d90f515e8581a8f948050
Iker Ibarguren <ikerib@gmail.com>
2016-09-22 13:41:10 +0200

    Kanalak

--- src/Zerbikat/BackendBundle/Controller/KanalaController.php
+++ src/Zerbikat/BackendBundle/Controller/KanalaController.php
@@ -31,36 +31,17 @@ class KanalaController extends Controller
         $auth_checker = $this->get('security.authorization_checker');
         if ($auth_checker->isGranted('ROLE_KUDEAKETA')) {
             $em = $this->getDoctrine()->getManager();
-//            $kanalas = $em->getRepository('BackendBundle:Kanala')->findAll();
             $kanalas = $em->getRepository('BackendBundle:Kanala')
                 ->findBy( array(), array('kanalmota'=>'ASC') );
 
-            $adapter = new ArrayAdapter($kanalas);
-            $pagerfanta = new Pagerfanta($adapter);
-            
             $deleteForms = array();
             foreach ($kanalas as $kanala) {
                 $deleteForms[$kanala->getId()] = $this->createDeleteForm($kanala)->createView();
             }
 
-            try {
-                $entities = $pagerfanta
-                    // Le nombre maximum d'éléments par page
-                    ->setMaxPerPage($this->getUser()->getUdala()->getOrrikatzea())
-                    // Notre position actuelle (numéro de page)
-                    ->setCurrentPage($page)
-                    // On récupère nos entités via Pagerfanta,
-                    // celui-ci s'occupe de limiter la requête en fonction de nos réglages.
-                    ->getCurrentPageResults()
-                ;
-            } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
-                throw $this->createNotFoundException("Orria ez da existitzen");
-            }            
-            
             return $this->render('kanala/index.html.twig', array(
-                'kanalas' => $entities,
-                'deleteforms' => $deleteForms,
-                'pager' => $pagerfanta,
+                'kanalas' => $kanalas,
+                'deleteforms' => $deleteForms
             ));
         }else
         {
