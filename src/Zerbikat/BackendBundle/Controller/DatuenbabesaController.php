79bbbd3dc042dfd5b2917577d49c63cc0d9be597
Iker Ibarguren <ikerib@gmail.com>
2016-09-22 13:38:29 +0200

    Datuen Babesa

--- src/Zerbikat/BackendBundle/Controller/DatuenbabesaController.php
+++ src/Zerbikat/BackendBundle/Controller/DatuenbabesaController.php
@@ -31,36 +31,17 @@ class DatuenbabesaController extends Controller
         $auth_checker = $this->get('security.authorization_checker');
         if ($auth_checker->isGranted('ROLE_KUDEAKETA')) {
             $em = $this->getDoctrine()->getManager();
-//            $datuenbabesas = $em->getRepository('BackendBundle:Datuenbabesa')->findAll();
             $datuenbabesas = $em->getRepository('BackendBundle:Datuenbabesa')
                 ->findBy( array(), array('kodea'=>'ASC') );
-
-            $adapter = new ArrayAdapter($datuenbabesas);
-            $pagerfanta = new Pagerfanta($adapter);
 
             $deleteForms = array();
             foreach ($datuenbabesas as $datuenbabesa) {
                 $deleteForms[$datuenbabesa->getId()] = $this->createDeleteForm($datuenbabesa)->createView();
             }
 
-            try {
-                $entities = $pagerfanta
-                    // Le nombre maximum d'éléments par page
-                    ->setMaxPerPage($this->getUser()->getUdala()->getOrrikatzea())
-                    // Notre position actuelle (numéro de page)
-                    ->setCurrentPage($page)
-                    // On récupère nos entités via Pagerfanta,
-                    // celui-ci s'occupe de limiter la requête en fonction de nos réglages.
-                    ->getCurrentPageResults()
-                ;
-            } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
-                throw $this->createNotFoundException("Orria ez da existitzen");
-            }
-
             return $this->render('datuenbabesa/index.html.twig', array(
-                'datuenbabesas' => $entities,
-                'deleteforms' => $deleteForms,
-                'pager' => $pagerfanta,
+                'datuenbabesas' => $datuenbabesas,
+                'deleteforms' => $deleteForms
             ));
         }else
         {
