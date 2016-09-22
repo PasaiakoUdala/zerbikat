c8fd9df27f6851dd1d5d2475d47545ac5fbf8745
Iker Ibarguren <ikerib@gmail.com>
2016-09-22 13:39:47 +0200

    Prozedurak

--- src/Zerbikat/BackendBundle/Controller/ProzeduraController.php
+++ src/Zerbikat/BackendBundle/Controller/ProzeduraController.php
@@ -33,32 +33,14 @@ class ProzeduraController extends Controller
             $em = $this->getDoctrine()->getManager();
             $prozeduras = $em->getRepository('BackendBundle:Prozedura')->findAll();
 
-            $adapter = new ArrayAdapter($prozeduras);
-            $pagerfanta = new Pagerfanta($adapter);
-
             $deleteForms = array();
             foreach ($prozeduras as $prozedura) {
                 $deleteForms[$prozedura->getId()] = $this->createDeleteForm($prozedura)->createView();
             }
 
-            try {
-                $entities = $pagerfanta
-                    // Le nombre maximum d'éléments par page
-                    ->setMaxPerPage($this->getUser()->getUdala()->getOrrikatzea())
-                    // Notre position actuelle (numéro de page)
-                    ->setCurrentPage($page)
-                    // On récupère nos entités via Pagerfanta,
-                    // celui-ci s'occupe de limiter la requête en fonction de nos réglages.
-                    ->getCurrentPageResults()
-                ;
-            } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
-                throw $this->createNotFoundException("Orria ez da existitzen");
-            }
-
             return $this->render('prozedura/index.html.twig', array(
-                'prozeduras' => $entities,
-                'deleteforms' => $deleteForms,
-                'pager' => $pagerfanta,
+                'prozeduras' => $prozeduras,
+                'deleteforms' => $deleteForms
             ));
         }else
         {
