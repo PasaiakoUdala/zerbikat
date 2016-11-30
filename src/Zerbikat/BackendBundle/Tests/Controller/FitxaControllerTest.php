210a346459355dc9985369a4a1c737fd8d103f57
ikerib <ikerib@gmail.com>
2016-11-30 11:05:12 +0100

    Tests

--- src/Zerbikat/BackendBundle/Tests/Controller/FitxaControllerTest.php
+++ src/Zerbikat/BackendBundle/Tests/Controller/FitxaControllerTest.php
@@ -3,53 +3,57 @@
 namespace Zerbikat\BackendBundle\Tests\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+use Symfony\Component\BrowserKit\Cookie;
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+
 
 class FitxaControllerTest extends WebTestCase
 {
-    /*
-    public function testCompleteScenario()
+    private $client = null;
+
+    public function setUp()
     {
-        // Create a new client to browse the application
-        $client = static::createClient();
-
-        // Create a new entry in the database
-        $crawler = $client->request('GET', '/fitxa/');
-        $this->assertEquals(200, $client->getResponse()->getStatusCode(), "Unexpected HTTP status code for GET /fitxa/");
-        $crawler = $client->click($crawler->selectLink('Create a new entry')->link());
-
-        // Fill in the form and submit it
-        $form = $crawler->selectButton('Create')->form(array(
-            'zerbikat_backendbundle_fitxa[field_name]'  => 'Test',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check data in the show view
-        $this->assertGreaterThan(0, $crawler->filter('td:contains("Test")')->count(), 'Missing element td:contains("Test")');
-
-        // Edit the entity
-        $crawler = $client->click($crawler->selectLink('Edit')->link());
-
-        $form = $crawler->selectButton('Update')->form(array(
-            'zerbikat_backendbundle_fitxa[field_name]'  => 'Foo',
-            // ... other fields to fill
-        ));
-
-        $client->submit($form);
-        $crawler = $client->followRedirect();
-
-        // Check the element contains an attribute with value equals "Foo"
-        $this->assertGreaterThan(0, $crawler->filter('[value="Foo"]')->count(), 'Missing element [value="Foo"]');
-
-        // Delete the entity
-        $client->submit($crawler->selectButton('Delete')->form());
-        $crawler = $client->followRedirect();
-
-        // Check the entity has been delete on the list
-        $this->assertNotRegExp('/Foo/', $client->getResponse()->getContent());
+        $this->client = static::createClient();
     }
 
-    */
+    public function testSecuredHello()
+    {
+        $this->logIn();
+
+        $crawler = $this->client->request('GET', '/eu/fitxa/');
+
+        $this->assertTrue($this->client->getResponse()->isSuccessful());
+        $this->assertGreaterThan(0, $crawler->filter('html:contains("Fitxak")')->count());
+
+
+
+        $link = $crawler->filter('a:contains("Fitxa berria")')->eq(1)->link();
+
+        $crawler = $client->click($link);
+
+
+//        $client = static::createClient();
+//        $crawler = $this->client->request('GET', '/eu/fitxa/new');
+//        $crawler = $client->click($crawler->selectLink('Fitxa berria')->link());
+//
+//        $link = $this->client->selectLink(' Fitxa berria')->link();
+//        $crawler = $client->click($link);
+//
+//        $form = $crawler->selectButton('validate')->form();
+//        $crawler = $client->submit($form, array('name' => 'Fabien'));
+    }
+
+    private function logIn()
+    {
+        $session = $this->client->getContainer()->get('session');
+
+        // the firewall context (defaults to the firewall name)
+        $firewall = 'main';
+        $token = new UsernamePasswordToken('pasaia', null, $firewall, array('ROLE_ADMIN'));
+        $session->set('_security_'.$firewall, serialize($token));
+        $session->save();
+
+        $cookie = new Cookie($session->getName(), $session->getId());
+        $this->client->getCookieJar()->set($cookie);
+    }
 }
