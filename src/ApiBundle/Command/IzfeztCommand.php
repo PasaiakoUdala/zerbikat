5372822cba938f5a24012eda3862138d1740297f
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2017-12-20 10:22:00 +0100

    'ZERBITZU' sartu 'SERVICIO' eta 'FITXAZERBIKAT' izan beharrean

--- src/ApiBundle/Command/IzfeztCommand.php
+++ src/ApiBundle/Command/IzfeztCommand.php
@@ -138,17 +138,14 @@ class IzfeztCommand extends ContainerAwareCommand
                     "UPM-CE",
                     "UPM-HE",
                     "URG",
-                    //"URA",
+                    "URA",
                     "URB",
                     "UVD",
                 );
 
-                if ( $tipo == "URA" ) {
-                    $A204TIPO = "'FITXAZERBIKAT'";
-                    $A204IDTIPO = "'" . $tipo . $IdPagina . "'";
-                } elseif ( in_array( $tipo, $servicios ) ) {
-                    $A204TIPO = "'SERVICIO'";
-                    $A204IDTIPO = "'" . $tipo . "'";
+                if ( in_array( $tipo, $servicios ) ) {
+                    $A204TIPO = "'ZERBITZU'";
+                    $A204IDTIPO = "'" . $IdPagina . "'";
                 } else {
                     $A204TIPO = "'PROPIA'";
                     $A204IDTIPO = "'" . $tipo . "'";
@@ -254,8 +251,8 @@ class IzfeztCommand extends ContainerAwareCommand
                     break;
 
                 case "URM":
-                    $A202SERVICIO = "'SERVICIO'";
-                    $A202LINKEXT = "'" . $tipo . "'";
+                    $A202SERVICIO = "'ZERBITZU'";
+                    $A202LINKEXT = "'" . $linkext . "'";
                     break;
 
                 default:
@@ -274,11 +271,11 @@ class IzfeztCommand extends ContainerAwareCommand
                     );
 
                     if ( $tipo == "URA" ) {
-                        $A202SERVICIO = "'FITXAZERBIKAT'";
-                        $A202LINKEXT = "'" . $tipo . $linkext . "'";
+                        $A202SERVICIO = "'ZERBITZU'";
+                        $A202LINKEXT = "'" . $linkext . "'";
                     } elseif ( in_array( $tipo, $servicios ) ) {
-                        $A202SERVICIO = "'SERVICIO'";
-                        $A202LINKEXT = "'" . $tipo . "'";
+                        $A202SERVICIO = "'ZERBITZU'";
+                        $A202LINKEXT = "'" . $linkext . "'";
                     } else {
                         $A202SERVICIO = "'EXPEDIENTE'";
                         $A202LINKEXT = "'" . $tipo . "'";
@@ -399,32 +396,35 @@ class IzfeztCommand extends ContainerAwareCommand
         $kanalmotak = $em->getRepository( 'BackendBundle:Kanalmota' )->findAll();
 
         $query = $em->createQuery(
+            /** @lang text */
             '
-                    SELECT f.oharraktext,f.helburuatext,f.ebazpensinpli,f.arduraaitorpena,f.aurreikusi,f.arrunta,f.isiltasunadmin,f.norkeskatutext,f.norkeskatutable,f.dokumentazioatext,f.dokumentazioatable,f.kostuatext,f.kostuatable,f.araudiatext,f.araudiatable,f.prozeduratext,f.prozeduratable,f.doklaguntext,f.doklaguntable,f.datuenbabesatext,f.datuenbabesatable,f.norkebatzitext,f.norkebatzitable,f.besteak1text,f.besteak1table,f.besteak2text,f.besteak2table,f.besteak3text,f.besteak3table,f.kanalatext,f.kanalatable,f.azpisailatable
-                      FROM BackendBundle:Eremuak f
-                      WHERE f.udala = :udala
-                '
+            SELECT f.oharraktext,f.helburuatext,f.ebazpensinpli,f.arduraaitorpena,f.aurreikusi,f.arrunta,f.isiltasunadmin,f.norkeskatutext,f.norkeskatutable,f.dokumentazioatext,f.dokumentazioatable,f.kostuatext,f.kostuatable,f.araudiatext,f.araudiatable,f.prozeduratext,f.prozeduratable,f.doklaguntext,f.doklaguntable,f.datuenbabesatext,f.datuenbabesatable,f.norkebatzitext,f.norkebatzitable,f.besteak1text,f.besteak1table,f.besteak2text,f.besteak2table,f.besteak3text,f.besteak3table,f.kanalatext,f.kanalatable,f.azpisailatable
+                FROM BackendBundle:Eremuak f
+                WHERE f.udala = :udala
+            '
         );
         $query->setParameter( 'udala', $udala->getId() );
         $eremuak = $query->getSingleResult();
 
         $query = $em->createQuery(
+            /** @lang text */
             '
-                  SELECT f.oharraklabeleu,f.oharraklabeles,f.helburualabeleu,f.helburualabeles,f.ebazpensinplilabeleu,f.ebazpensinplilabeles,f.arduraaitorpenalabeleu,f.arduraaitorpenalabeles,f.aurreikusilabeleu,f.aurreikusilabeles,f.arruntalabeleu,f.arruntalabeles,f.isiltasunadminlabeleu,f.isiltasunadminlabeles,f.norkeskatulabeleu,f.norkeskatulabeles,f.dokumentazioalabeleu,f.dokumentazioalabeles,f.kostualabeleu,f.kostualabeles,f.araudialabeleu,f.araudialabeles,f.prozeduralabeleu,f.prozeduralabeles,f.doklagunlabeleu,f.doklagunlabeles,f.datuenbabesalabeleu,f.datuenbabesalabeles,f.norkebatzilabeleu,f.norkebatzilabeles,f.besteak1labeleu,f.besteak1labeles,f.besteak2labeleu,f.besteak2labeles,f.besteak3labeleu,f.besteak3labeles,f.kanalalabeleu,f.kanalalabeles,f.epealabeleu,f.epealabeles,f.doanlabeleu,f.doanlabeles,f.azpisailalabeleu,f.azpisailalabeles
+                SELECT f.oharraklabeleu,f.oharraklabeles,f.helburualabeleu,f.helburualabeles,f.ebazpensinplilabeleu,f.ebazpensinplilabeles,f.arduraaitorpenalabeleu,f.arduraaitorpenalabeles,f.aurreikusilabeleu,f.aurreikusilabeles,f.arruntalabeleu,f.arruntalabeles,f.isiltasunadminlabeleu,f.isiltasunadminlabeles,f.norkeskatulabeleu,f.norkeskatulabeles,f.dokumentazioalabeleu,f.dokumentazioalabeles,f.kostualabeleu,f.kostualabeles,f.araudialabeleu,f.araudialabeles,f.prozeduralabeleu,f.prozeduralabeles,f.doklagunlabeleu,f.doklagunlabeles,f.datuenbabesalabeleu,f.datuenbabesalabeles,f.norkebatzilabeleu,f.norkebatzilabeles,f.besteak1labeleu,f.besteak1labeles,f.besteak2labeleu,f.besteak2labeles,f.besteak3labeleu,f.besteak3labeles,f.kanalalabeleu,f.kanalalabeles,f.epealabeleu,f.epealabeles,f.doanlabeleu,f.doanlabeles,f.azpisailalabeleu,f.azpisailalabeles
                     FROM BackendBundle:Eremuak f
                     WHERE f.udala = :udala
-                '
+            '
         );
         $query->setParameter( 'udala', $udala->getId() );
         $labelak = $query->getSingleResult();
 
         $query = $em->createQuery(
+            /** @lang text */
             '
-                  SELECT f 
+                SELECT f 
                     FROM BackendBundle:Familia f 
-                      LEFT JOIN BackendBundle:Udala u WITH f.udala=u.id                      
-                  WHERE u.kodea = :udala AND f.parent IS NULL 
-                  ORDER BY f.ordena ASC
+                        LEFT JOIN BackendBundle:Udala u WITH f.udala=u.id                      
+                    WHERE u.kodea = :udala AND f.parent IS NULL 
+                    ORDER BY f.ordena ASC
                 '
         );
         $query->setParameter( 'udala', $udalKodea );
@@ -500,11 +500,7 @@ class IzfeztCommand extends ContainerAwareCommand
                 /**************************************************************************************************/
                 if ( 1 == 1 ) { // Folding egiteko addFitxa bezala refaktorizatu beharko zen
 
-                    if ( !in_array(
-                        $fitxa->getEspedientekodea(),
-                        $sortutakoFitxak
-                    )
-                    ) { // Begiratu ea aldez aurretik sortu dugun...
+                    if ( !in_array($fitxa->getEspedientekodea(),$sortutakoFitxak)) { // Begiratu ea aldez aurretik sortu dugun...
                         $kostuZerrenda = array();
                         foreach ( $fitxa->getKostuak() as $kostu ) {
                             $api = $this->getContainer()->getParameter( 'zzoo_aplikazioaren_API_url' );
@@ -876,7 +872,7 @@ class IzfeztCommand extends ContainerAwareCommand
                                             }
                                             if ( $kanala->getTelematikoa() ) {
                                                 if ( $fitxa->getZerbitzua() ) {
-                                                    if (( strlen($fitxa->getExpedientes()) > 0 )  && ( substr( $fitxa->getParametroa(),0,2) == "08" )) {
+                                                    if ( ( strlen( $fitxa->getExpedientes() ) > 0 ) && ( substr( $fitxa->getParametroa(), 0, 2 ) == "08" ) ) {
                                                         $textes = $textes . "<li><a href='" . $fitxa->getZerbitzua()->getErroaes() . $fitxa->getUdala()->getKodea() . $fitxa->getParametroa() . $fitxa->getExpedientes() . "' target='_blank'>" . $kanala->getIzenaes() . "</a></li>";
                                                         $texteu = $texteu . "<li><a href='" . $fitxa->getZerbitzua()->getErroaeu() . $fitxa->getUdala()->getKodea() . $fitxa->getParametroa() . $fitxa->getExpedientes() . "' target='_blank'>" . $kanala->getIzenaeu() . "</a></li>";
                                                     } else {
@@ -2528,7 +2524,7 @@ class IzfeztCommand extends ContainerAwareCommand
                                                 }
                                                 if ( $kanala->getTelematikoa() ) {
                                                     if ( $fitxa->getZerbitzua() ) {
-                                                        if (( strlen($fitxa->getExpedientes()) > 0 )  && ( substr( $fitxa->getParametroa(),0,2) == "08" )) {
+                                                        if ( ( strlen( $fitxa->getExpedientes() ) > 0 ) && ( substr( $fitxa->getParametroa(), 0, 2 ) == "08" ) ) {
                                                             $textes = $textes . "<li><a href='" . $fitxa->getZerbitzua()->getErroaes() . $fitxa->getUdala()->getKodea() . $fitxa->getParametroa() . $fitxa->getExpedientes() . "' target='_blank'>" . $kanala->getIzenaes() . "</a></li>";
                                                             $texteu = $texteu . "<li><a href='" . $fitxa->getZerbitzua()->getErroaeu() . $fitxa->getUdala()->getKodea() . $fitxa->getParametroa() . $fitxa->getExpedientes() . "' target='_blank'>" . $kanala->getIzenaeu() . "</a></li>";
                                                         } else {
