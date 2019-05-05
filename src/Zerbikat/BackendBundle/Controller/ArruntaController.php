80313dd70f8ec4a3c21faf61d038bbf5f2fe39f3
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-05-05 21:58:48 +0200

    Arrunta pagination

--- src/Zerbikat/BackendBundle/Controller/ArruntaController.php
+++ src/Zerbikat/BackendBundle/Controller/ArruntaController.php
@@ -26,37 +26,22 @@ class ArruntaController extends Controller
      * @Route("/page{page}", name="arrunta_index_paginated")
      * @Method("GET")
      */
-    public function indexAction($page)
+    public function indexAction()
     {
         $auth_checker = $this->get('security.authorization_checker');
         if ($auth_checker->isGranted('ROLE_KUDEAKETA')) {
             $em = $this->getDoctrine()->getManager();
             $arruntas = $em->getRepository('BackendBundle:Arrunta')->findAll();
 
-            $adapter = new ArrayAdapter($arruntas);
-            $pagerfanta = new Pagerfanta($adapter);
 
             $deleteForms = array();
             foreach ($arruntas as $arrunta) {
                 $deleteForms[$arrunta->getId()] = $this->createDeleteForm($arrunta)->createView();
             }
-            try {
-                $entities = $pagerfanta
-                    // Le nombre maximum d'éléments par page
-//                    ->setMaxPerPage($this->getUser()->getUdala()->getOrrikatzea())
-                    // Notre position actuelle (numéro de page)
-                    ->setCurrentPage($page)
-                    // On récupère nos entités via Pagerfanta,
-                    // celui-ci s'occupe de limiter la requête en fonction de nos réglages.
-                    ->getCurrentPageResults()
-                ;
-            } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
-                throw $this->createNotFoundException("Orria ez da existitzen");
-            }
+
             return $this->render('arrunta/index.html.twig', array(
-                'arruntas' => $entities,
-                'deleteforms' => $deleteForms,
-                'pager' => $pagerfanta,
+                'arruntas' => $arruntas,
+                'deleteforms' => $deleteForms
             ));
         }else
         {
