1f48cb013b909ae6c499f1cfefc58c3ab54313ab
Iker Ibarguren Berasaluze <ikerib@gmail.com>
2019-05-05 22:02:24 +0200

    isiltasun administratiboa pagination

--- src/Zerbikat/BackendBundle/Controller/AurreikusiController.php
+++ src/Zerbikat/BackendBundle/Controller/AurreikusiController.php
@@ -34,32 +34,14 @@ class AurreikusiController extends Controller
             $em = $this->getDoctrine()->getManager();
             $aurreikusis = $em->getRepository('BackendBundle:Aurreikusi')->findAll();
 
-            $adapter = new ArrayAdapter($aurreikusis);
-            $pagerfanta = new Pagerfanta($adapter);
-
             $deleteForms = array();
             foreach ($aurreikusis as $aurreikusi) {
                 $deleteForms[$aurreikusi->getId()] = $this->createDeleteForm($aurreikusi)->createView();
             }
 
-            try {
-                $entities = $pagerfanta
-                    // Le nombre maximum d'éléments par page
-//                    ->setMaxPerPage($this->getUser()->getUdala()->getOrrikatzea())
-                    // Notre position actuelle (numéro de page)
-                    ->setCurrentPage($page)
-                    // On récupère nos entités via Pagerfanta,
-                    // celui-ci s'occupe de limiter la requête en fonction de nos réglages.
-                    ->getCurrentPageResults()
-                ;
-            } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
-                throw $this->createNotFoundException("Orria ez da existitzen");
-            }
-
             return $this->render('aurreikusi/index.html.twig', array(
-                'aurreikusis' => $entities,
-                'deleteforms' => $deleteForms,
-                'pager' => $pagerfanta,
+                'aurreikusis' => $aurreikusis,
+                'deleteforms' => $deleteForms
             ));
         }else
         {
--- src/Zerbikat/BackendBundle/Controller/IsiltasunAdministratiboaController.php
+++ src/Zerbikat/BackendBundle/Controller/IsiltasunAdministratiboaController.php
@@ -27,7 +27,7 @@ class IsiltasunAdministratiboaController extends Controller
      * @Route("/page{page}", name="isiltasunadministratiboa_index_paginated")
      * @Method("GET")
      */
-    public function indexAction($page)
+    public function indexAction()
     {
         $auth_checker = $this->get('security.authorization_checker');
         if ($auth_checker->isGranted('ROLE_KUDEAKETA'))
@@ -35,32 +35,14 @@ class IsiltasunAdministratiboaController extends Controller
             $em = $this->getDoctrine()->getManager();
             $isiltasunAdministratiboas = $em->getRepository('BackendBundle:IsiltasunAdministratiboa')->findAll();
 
-            $adapter = new ArrayAdapter($isiltasunAdministratiboas);
-            $pagerfanta = new Pagerfanta($adapter);
-            
             $deleteForms = array();
             foreach ($isiltasunAdministratiboas as $isiltasunAdministratiboa) {
                 $deleteForms[$isiltasunAdministratiboa->getId()] = $this->createDeleteForm($isiltasunAdministratiboa)->createView();
             }
 
-            try {
-                $entities = $pagerfanta
-                    // Le nombre maximum d'éléments par page
-//                    ->setMaxPerPage($this->getUser()->getUdala()->getOrrikatzea())
-                    // Notre position actuelle (numéro de page)
-                    ->setCurrentPage($page)
-                    // On récupère nos entités via Pagerfanta,
-                    // celui-ci s'occupe de limiter la requête en fonction de nos réglages.
-                    ->getCurrentPageResults()
-                ;
-            } catch (\Pagerfanta\Exception\NotValidCurrentPageException $e) {
-                throw $this->createNotFoundException("Orria ez da existitzen");
-            }
-            
             return $this->render('isiltasunadministratiboa/index.html.twig', array(
-                'isiltasunAdministratiboas' => $entities,
-                'deleteforms' => $deleteForms,
-                'pager' => $pagerfanta,
+                'isiltasunAdministratiboas' => $isiltasunAdministratiboas,
+                'deleteforms' => $deleteForms
             ));
         }else
         {
